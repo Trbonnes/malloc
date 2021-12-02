1be2e1c9624e7f205f9b11dccf427ba7e15dcc99
Trbonnes <trbonnes@student.42.fr>
2021-12-02 10:45:47 +0100

    added bool enum

--- includes/malloc.h
+++ includes/malloc.h
@@ -5,6 +5,18 @@
 #include <unistd.h>
 #include <pthread.h>
 
+typedef enum	e_bool {
+	FALSE,
+	TRUE
+}				t_bool;
+typedef struct s_block {
+    size_t data_size;
+    t_bool freed;
+
+    struct s_block *next;
+    struct s_block *prev;
+
+}   t_block;
 typedef struct s_page {
 
     size_t total_size;
@@ -14,26 +26,25 @@ typedef struct s_page {
     struct s_page *next;
     struct s_page *prev;
 
+    struct s_block *blocks;
+
 }   t_page;
 
-typedef struct s_block {
-    size_t data_size;
-    bool freed;
-
-    struct s_block *next;
-    struct s_block *prev;
-
-}   t_block;
 
 #define PAGE_SHIFT(start)((void *)start + sizeof(t_page))
 #define BLOCK_SHIFT(start)((void *)start + sizeof(t_block))
 
 /*
 For tiny blocks, let’s say we use 128 bytes for a maximum malloc size.
-If we fill a heap with 128 of them, it gives us a TINY_HEAP_ALLOCATION_SIZE of 16KB (128 * 128).
+If we fill a page with 128 of them, it gives us a TINY_ALLOCATION_SIZE of 16KB (128 * 128).
 Since each malloc has to store its metadata (sizeof(t_block) = 32 bytes), we won’t be able to store all the 128 blocks.
-16 KB / (128 + 32) = 102.4. So, counting the t_heapat the start of the 16KB heap , we can store 102 128 bytes mallocs
+16 KB / (128 + 32) = 102.4. So, counting the t_page at the start of the 16KB heap , we can store 102 times a 128 bytes malloc
 */
+
+#define TINY_ALLOCATION_SIZE (4 * getpagesize())
+#define TINY_BLOCK_SIZE (TINY_ALLOCATION_SIZE / 128)
+#define SMALL_ALLOCATION_SIZE (16 * getpagesize())
+#define SMALL_BLOCK_SIZE (SMALL_ALLOCATION_SIZE / 128)
 
 void free(void *ptr);
 void *malloc(size_t size);
